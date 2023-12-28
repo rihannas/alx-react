c7b52514f59d14bbc59f72f6a6d09b11fca9b62a
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2023-12-29 02:43:31 +0300

    done task 3

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -9,10 +9,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
     <div className='root-notifications'>
       <Notifications/>
     </div>
+    <App />
   </React.StrictMode>
 );
 
--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -9,3 +9,6 @@ export function getFooterCopy(isIndex){
     return isIndex ? 'Holberton School' : 'Holberton School main dashboard'
 }
 
+export const getLatestNotification = () =>{
+    return '<strong>Urgent requirement</strong> - complete by EOD'
+}
